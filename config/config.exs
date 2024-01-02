5e90a122ce22f3dc5c8144123d2171f6875599f3
Nicholas Roberts <niccolox@devekko.com>
2024-01-01 20:26:50 -0800

    selenium otp update

--- config/config.exs
+++ config/config.exs
@@ -59,6 +59,11 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Selenium
+config :wallaby, driver: Wallaby.Selenium
+
+config :wallaby, otp_app: :phoenix_e2e
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
