abe35e8bf82f7c548b7797e7de8bef67786f5a93
Nicholas Roberts <niccolox@devekko.com>
2024-01-01 21:32:35 -0800

    env

--- config/prod.exs
+++ config/prod.exs
@@ -19,3 +19,12 @@ config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
+
+config :phoenix_e2e, PhoenixE2e.Repo,
+  username: System.get_env("DB_USERNAME"),
+  password: System.get_env("DB_PASSWORD"),
+  hostname: System.get_env("DB_HOSTNAME"),
+  database: System.get_env("DB_DATABASE")",
+  stacktrace: true,
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 10
