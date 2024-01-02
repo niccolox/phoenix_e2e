1eda775daacc50872c63bbdbf83216a3694ed093
Nicholas Roberts <niccolox@devekko.com>
2024-01-02 00:15:15 -0800

    PhoenixE2eWeb.Endpoint

--- test/phoenix_e2e_web/controllers/error_html_test.exs
+++ test/phoenix_e2e_web/controllers/error_html_test.exs
@@ -11,4 +11,22 @@ defmodule PhoenixE2eWeb.ErrorHTMLTest do
   test "renders 500.html" do
     assert render_to_string(PhoenixE2eWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
   end
+
+  use Wallaby.Integration.SessionCase, async: true
+
+  setup %{session: session} do
+    page = visit(session, "/")
+    {:ok, %{page: page}}
+  end
+
+  @h1 Query.css("h1")
+
+  describe "has_text/3" do
+    test "checks if the query has the specified text", %{page: page} do
+      assert page
+             |> has_text?(@h1, "Phoenix Framework")
+    end
+  end
+
+
 end
