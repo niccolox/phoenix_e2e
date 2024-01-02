d2631bfd92505abd034836a6e175a8e40d184f54
Nicholas Roberts <niccolox@devekko.com>
2024-01-01 23:31:05 -0800

    wallaby config

--- config/config.exs
+++ config/config.exs
@@ -62,3 +62,8 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+# Selenium
+config :wallaby, driver: Wallaby.Selenium
+
+config :wallaby, otp_app: :phoenix_e2e
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,3 @@
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(PhoenixE2e.Repo, :manual)
+{:ok, _} = Application.ensure_all_started(:wallaby)
