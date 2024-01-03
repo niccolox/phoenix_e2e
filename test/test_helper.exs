113072cb30bfb4fc8b8bdcde97102508ba75bd52
Nicholas Roberts <niccolox@devekko.com>
2024-01-02 20:16:43 -0800

    browser dispatch

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,3 +2,4 @@ ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(PhoenixE2e.Repo, :manual)
 {:ok, _} = Application.ensure_all_started(:wallaby)
 Application.put_env(:wallaby, :base_url, PhoenixE2eWeb.Endpoint.url())
+
